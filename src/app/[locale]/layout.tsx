b4995f77f4d239101fa7addd6904814aa06b906d
Alexandre Djun Igari <djun_igari@hotmail.com>
2023-05-27 11:55:42 +0900

    Fixing positions of layout components

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -37,52 +37,46 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale}>
-      <body className="theme-dark theme-gray h-screen bg-primaryBg text-onPrimaryBg overflow-hidden">
+      <body className="theme-dark theme-gray h-screen max-h-screen bg-primaryBg text-onPrimaryBg overflow-scroll">
         <div
           className={`
           fixed
           w-full
           h-full
           -inset-0
+          -z-10
           bg-gradient-to-br from-gray-900 to-gray-600 opacity-80
         `}
         ></div>
-        <div
-          id="main-content"
-          className="relative h-screen w-full overflow-scroll"
-        >
-          <NextIntlClientProvider locale={locale} messages={messages}>
-            <Navbar />
-            <main className="max-w-3xl px-4 md:mx-auto ">{children}</main>
-          </NextIntlClientProvider>
-          <ContactButton />
-          <BackToTopButton />
-          <footer className="flex flex-row gap-4 justify-center items-center p-10">
-            <Link
-              href={
-                'https://www.linkedin.com/in/alexandre-djun-igari-91a03065/'
-              }
-              target="_blank"
-              rel="noreferrer"
-            >
-              <BsLinkedin className="h-6 w-6 md:h-8 md:w-8 cursor-pointer hover:text-onHoverPrimaryBg" />
-            </Link>
-            <Link
-              href={'https://github.com/djunigari'}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <BsGithub className="h-6 w-6 md:h-8 md:w-8 cursor-pointer hover:text-onHoverPrimaryBg" />
-            </Link>
-            <Link
-              href={'/resume/resume-en.pdf'}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <HiDocumentText className="h-6 w-6 md:h-8 md:w-8 cursor-pointer hover:text-onHoverPrimaryBg" />
-            </Link>
-          </footer>
-        </div>
+
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <Navbar />
+          <main id="main-content" className="max-w-3xl px-4 md:mx-auto ">
+            {children}
+          </main>
+        </NextIntlClientProvider>
+
+        <ContactButton />
+        <BackToTopButton />
+        <footer className="flex flex-row gap-4 justify-center items-center p-10">
+          <Link
+            href={'https://www.linkedin.com/in/alexandre-djun-igari-91a03065/'}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <BsLinkedin className="h-6 w-6 md:h-8 md:w-8 cursor-pointer hover:text-onHoverPrimaryBg" />
+          </Link>
+          <Link
+            href={'https://github.com/djunigari'}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <BsGithub className="h-6 w-6 md:h-8 md:w-8 cursor-pointer hover:text-onHoverPrimaryBg" />
+          </Link>
+          <Link href={'/resume/resume-en.pdf'} target="_blank" rel="noreferrer">
+            <HiDocumentText className="h-6 w-6 md:h-8 md:w-8 cursor-pointer hover:text-onHoverPrimaryBg" />
+          </Link>
+        </footer>
       </body>
     </html>
   )
--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -5,19 +5,22 @@ import { NavMenu } from './NavMenu'
 
 export default function Navbar() {
   return (
-    <nav
-      className={`
-        sticky top-0 z-10 w-full bg-primaryBg px-2  mx-auto max-w-7xl 
+    <>
+      <nav
+        className={`
+        fixed top-0 z-10 w-full bg-primaryBg px-2  mx-auto max-w-7xl 
         flex h-16 gap-3 items-center
         sm:bg-opacity-30 hover:bg-opacity-100 sm:px-6
         lg:px-8
       `}
-    >
-      <NavMenu />
+      >
+        <NavMenu />
 
-      <LanguageDropDown className="ml-auto" />
-      <ThemeSwitch />
-      <ModeSwitch />
-    </nav>
+        <LanguageDropDown className="ml-auto" />
+        <ThemeSwitch />
+        <ModeSwitch />
+      </nav>
+      <div className="h-16 w-full"></div>
+    </>
   )
 }
