18769bb91df094f73c29e89c74cce8d4c7b2a6b1
Alexandre Djun Igari <djun_igari@hotmail.com>
2024-06-16 19:55:09 +0900

    change to postgresql

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -3,6 +3,12 @@ import { PrismaClient } from '@prisma/client'
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined
 }
+const { DATABASE_URL, CA_CERT_PATH } = process.env
+
+// Adicionar o certificado CA à URL do banco de dados
+const dsnWithCert = `${DATABASE_URL}&sslrootcert=${CA_CERT_PATH}`
+
+process.env.DATABASE_URL_WITH_CERT = dsnWithCert
 
 export const prisma =
   globalForPrisma.prisma ??
